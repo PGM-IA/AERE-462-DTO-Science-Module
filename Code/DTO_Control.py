1d4fc0a9877dfa20b528baaa2daec087b1705b54
PGM-IA <pgm@iastate.edu>
2022-10-12 20:31:46 -0500

    Started scripting control system

--- Code/DTO_Control.py
+++ Code/DTO_Control.py
@@ -2,3 +2,28 @@
 # AERE 462
 # DTO Code 
 
+# TODO Implement control binds and set associated gpio pin outputs to control thrust and pulses
+
+from pynput.keyboard import Key, Listener
+
+def show(key):
+
+    try:
+        print('alphanumeric key {0} pressed'.format(key.char))
+
+    except AttributeError:
+
+        # When delete key is pressed terminate the control system connection
+
+        if key == Key.delete:      
+            return False
+
+
+    print('\nYou Entered {0}'.format(key))
+    print(key)
+
+    
+
+# Collect all inputs until termination
+with Listener(on_press = show) as listener:
+	listener.join()
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 This Github page acts as repo of all of our infomation/material created for this class. 
 This includes our solidworks models, code used to control the DTO and other material used in the making of the DTO for this class.
 
+Current Dependencies:
+    [Pynput](https://github.com/moses-palmer/pynput)
